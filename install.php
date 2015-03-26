c63017b90ae978f22a8cbe6f1afae4a2bd0fa0ee
Sergey Pryadkin <GiperProger@gmail.com>
2015-03-26 15:10:40 +0600

    alfa version of geolocation data plugin

---
+++ install.php
@@ -0,0 +1,42 @@
+<?php
+
+$dbPrefix = OW_DB_PREFIX;
+$plugin = OW::getPluginManager()->getPlugin('geolocationdata');
+OW::getLanguage()->importPluginLangs($plugin->getRootDir() . 'langs.zip', 'geolocationdata');
+$sqlFile = OW::getPluginManager()->getPlugin( 'geolocationdata' )->getRootDir()."ow_geolocationdata_ipv4.sql";
+
+ if ( !($fd = @fopen($sqlFile, 'rb')) ) {
+            throw new LogicException('SQL dump file `'.$sqlFile.'` not found');
+        }
+
+ $lineNo = 0;
+        $query = '';
+        while ( false !== ($line = fgets($fd, 10240)) )
+        {
+            $lineNo++;
+
+            if ( !strlen(($line = trim($line)))
+                || $line{0} == '#' || $line{0} == '-'
+                || preg_match('~^/\*\!.+\*/;$~siu', $line) ) {
+                continue;
+            }
+
+            $query .= $line;
+
+            if ( $line{strlen($line)-1} != ';' ) {
+                continue;
+            }
+
+            $query = str_replace('%%TBL-PREFIX%%', OW_DB_PREFIX, $query);
+
+            try {
+                OW::getDbo()->query($query);
+            }
+            catch ( Exception $e ) {
+                throw new LogicException('<b>ow_includes/config.php</b> file is incorrect. Update it with details provided below.');
+            }
+
+            $query = '';
+        }
+
+        fclose($fd);
