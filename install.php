95c221e2c0e7f024f94469a6a31300002607f260
Azazel <azazel9966@gmail.com>
2021-04-07 14:48:22 +0600

    Fix install Geolocation IPs database

--- install.php
+++ install.php
@@ -14,7 +14,17 @@ $dbPrefix = OW_DB_PREFIX;
 $dbo = OW::getDbo();
 $logger = OW::getLogger();
 
-// add absent columns (fix for https://github.com/skalfa/workflow/issues/1223)
+$sql =
+    "CREATE TABLE IF NOT EXISTS `{$dbPrefix}base_geolocation_country` (
+  `id` int(11) NOT NULL auto_increment,
+  `cc2` char(2) NOT NULL,
+  `cc3` char(3) NOT NULL,
+  `name` varchar(50) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8";
+
+OW::getDbo()->query($sql);
+
 try
 {
     $query = "SHOW COLUMNS FROM `{$dbPrefix}base_geolocation_ip_to_country` LIKE 'ipFrom'";
@@ -38,17 +48,17 @@ catch (Exception $e)
 
 try
 {
-    $query = "SHOW COLUMNS FROM `{$tblPrefix}base_geolocation_ip_to_country` LIKE 'ipTo'";
+    $query = "SHOW COLUMNS FROM `{$dbPrefix}base_geolocation_ip_to_country` LIKE 'ipTo'";
     $column = $dbo->queryForRow($query);
 
     if ( !$column )
     {
-        $query = "ALTER TABLE `{$tblPrefix}base_geolocation_ip_to_country` ADD `ipTo` bigint UNSIGNED";
+        $query = "ALTER TABLE `{$dbPrefix}base_geolocation_ip_to_country` ADD `ipTo` bigint UNSIGNED";
         $dbo->query($query);
     }
     else
     {
-        $query = "ALTER TABLE `{$tblPrefix}base_geolocation_ip_to_country` CHANGE `ipTo` `ipTo` bigint UNSIGNED";
+        $query = "ALTER TABLE `{$dbPrefix}base_geolocation_ip_to_country` CHANGE `ipTo` `ipTo` bigint UNSIGNED";
         $dbo->query($query);
     }
 }
@@ -60,7 +70,7 @@ catch (Exception $e)
 // add index
 try
 {
-    $query = "ALTER TABLE `{$tblPrefix}base_geolocation_ip_to_country` ADD INDEX `ipRange` (`ipFrom`, `ipTo`)";
+    $query = "ALTER TABLE `{$dbPrefix}base_geolocation_ip_to_country` ADD INDEX `ipRange` (`ipFrom`, `ipTo`)";
     $dbo->query($query);
 
 }
@@ -73,39 +83,39 @@ $plugin = OW::getPluginManager()->getPlugin('geolocationdata');
 OW::getLanguage()->importPluginLangs($plugin->getRootDir() . 'langs.zip', 'geolocationdata');
 $sqlFile = OW::getPluginManager()->getPlugin( 'geolocationdata' )->getRootDir()."ow_geolocationdata_ipv4.sql";
 
- if ( !($fd = @fopen($sqlFile, 'rb')) ) {
-            throw new LogicException('SQL dump file `'.$sqlFile.'` not found');
-        }
+if ( !($fd = @fopen($sqlFile, 'rb')) ) {
+    throw new LogicException('SQL dump file `'.$sqlFile.'` not found');
+}
 
- $lineNo = 0;
-        $query = '';
-        while ( false !== ($line = fgets($fd, 10240)) )
-        {
-            $lineNo++;
+$lineNo = 0;
+$query = '';
+while ( false !== ($line = fgets($fd, 10240)) )
+{
+    $lineNo++;
 
-            if ( !strlen(($line = trim($line)))
-                || $line{0} == '#' || $line{0} == '-'
-                || preg_match('~^/\*\!.+\*/;$~siu', $line) ) {
-                continue;
-            }
+    if ( !strlen(($line = trim($line)))
+        || $line{0} == '#' || $line{0} == '-'
+        || preg_match('~^/\*\!.+\*/;$~siu', $line) ) {
+        continue;
+    }
 
-            $query .= $line;
+    $query .= $line;
 
-            if ( $line{strlen($line)-1} != ';' ) {
-                continue;
-            }
+    if ( $line{strlen($line)-1} != ';' ) {
+        continue;
+    }
 
-            $query = str_replace('%%TBL-PREFIX%%', OW_DB_PREFIX, $query);
+    $query = str_replace('%%TBL-PREFIX%%', OW_DB_PREFIX, $query);
 
-            try {
-                OW::getDbo()->query($query);
-            }
-            catch ( Exception $e ) {
-                throw new LogicException('<b>ow_includes/config.php</b> file is incorrect. Update it with details provided below.');
-            }
+    try {
+        OW::getDbo()->query($query);
+    }
+    catch ( Exception $e ) {
+        throw new LogicException('<b>ow_includes/config.php</b> file is incorrect. Update it with details provided below.');
+    }
 
-            $query = '';
-        }
+    $query = '';
+}
 
-        fclose($fd);
+fclose($fd);
 
